0701f3d9e610f813bdbc3cf594185686d22b5594
pavi040315 <pavithran040315@gmail.com>
2025-10-21 10:18:10 +0800

    base program

--- program.py
+++ program.py
@@ -4,6 +4,7 @@
 
 import face_recognition
 import cv2 #######
+import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation   
 import numpy as np
 import os
@@ -14,14 +15,23 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+segmentor = SelfiSegmentation()
+
+background_img = cv2.imread("sunset.jpg")
+background_img = cv2.resize(background_img, (640, 480))
+
+
 while True:
     _, frame = capture.read()
 
-    cv2.imshow("Webcam", frame)
+    remBG = segmentor.removeBG(frame, background_img, 0.9)
+    imageStack = cvzone.stackImages([frame, remBG], 2, 1)
+
+    cv2.imshow("BG Remover", imageStack)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):   
         break
-    
+
 
 capture.release()
 cv2.destroyAllWindows()
